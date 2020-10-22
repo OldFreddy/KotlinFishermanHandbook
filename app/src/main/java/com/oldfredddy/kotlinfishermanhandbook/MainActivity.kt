0c4e1de6ccdcfdfd8797dced00793ab2170b1556
Freddy <oldfredddy@gmail.com>
2020-10-22 11:11:44 +0300

    RecyclerView

--- app/src/main/java/com/oldfredddy/kotlinfishermanhandbook/MainActivity.kt
+++ app/src/main/java/com/oldfredddy/kotlinfishermanhandbook/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.oldfredddy.kotlinfishermanhandbook
 
+import MyAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_layout.*
+import kotlinx.android.synthetic.main.main_content.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        nav_view.setNavigationItemSelectedListener(this)
+        nav_view.setNavigationItemSelectedListener (this)
+
+        var list = ArrayList<ListItem>()
+
+        list.add(ListItem(R.drawable.som,"Сом","чварчрпчаптчапт чапрапр ыарпчапрча"))
+        list.add(ListItem(R.drawable.nalim,"Налим","чварчрпчаптчапт чапрапр ыарпчапрча"))
+        list.add(ListItem(R.drawable.shuca,"Щука","чварчрпчаптчапт чапрапр ыарпчапрча"))
+        list.add(ListItem(R.drawable.caras,"Карась","чварчрпчаптчапт чапрапр ыарпчапрча"))
+        list.add(ListItem(R.drawable.som,"Карась","чварчрпчаптчапт чапрапр ыарпчапрча"))
+        rcView.hasFixedSize()
+        rcView.layoutManager = LinearLayoutManager(this)
+        rcView.adapter = MyAdapter(list,this)
+
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
